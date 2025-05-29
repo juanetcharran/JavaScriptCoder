14da0b2819407de82af075d5b54f4b1d68d05e81
juan manuel etcharran <jetcharran@gmail.com>
2025-05-29 01:10:07 -0300

    JS simulador finalizado

--- ej2.js
+++ ej2.js
@@ -35,7 +35,7 @@ function menu() {
         switch (selectMenu) {
             case 1:
                 alert("Empezá a comprar!");
-                buy(); //ver si está bien poner acá el código
+                buy(); 
                 break;
             case 2:
                 flag = false;
@@ -50,16 +50,21 @@ function menu() {
 function buy() {
     let flagTwo = true;
     while (flagTwo) {
-        let promptResponse = prompt("Elegí el tipo de pedido: 1-Promociones 2-Bebidas sueltas 3-Volver para atrás");
+        let promptResponse = prompt("Elegí el tipo de pedido: 1-Promociones 2-Bebidas sueltas 3-Ver carrito 4-Volver para atrás");
         let selectBuy = Number(promptResponse);
         switch (selectBuy) {
             case 1:
                 alert("Mirá nuestras promociones!");
+                selectPromo();
                 break;
             case 2:
                 alert("Elegí tus opciones de bebidas!");
+                selectDrink();
+                break;
+            case 3: 
+                seeCart();
                 break;    
-            case 3:
+            case 4:
                 flagTwo = false;
                 break;
             default:
@@ -70,25 +75,77 @@ function buy() {
     // armar combos e items
 }
 
-function buyOrder() {
+function selectPromo() {
+    let message = "Elegí tu promoción\n";
+    for (let i = 0; i < promo.length; i++) {
+        message += `${i + 1}. ${promo[i].nombre} - $${promo[i].precio}\n`;
+    }
+
+
+    let promptResponse = prompt(message);
+    if (isNaN(promptResponse) || promptResponse <= 0 ||  promptResponse >= promo.length + 1) {
+        alert("Tenés que indicar una opción correcta")
+    }
+    else {
+       cart.selections.push(promo[promptResponse - 1]); 
+    }
 
 }
 
+function selectDrink() {
+    let message = "Elegí tu bebida\n";
+    for (let i = 0; i < drink.length; i++) {
+        message += `${i + 1}. ${drink[i].nombre} - $${drink[i].precio}\n`;
+    }
+
+
+    let promptResponse = prompt(message);
+    if (isNaN(promptResponse) || promptResponse <= 0 ||  promptResponse >= drink.length + 1) {
+        alert("Tenés que indicar una opción correcta")
+    }
+    else {
+       cart.selections.push(drink[promptResponse - 1]); 
+    }
+
+}
+
+function seeCart() {
+   let message = "Te mostramos el resumen de tu pedido\n";
+   for (let i = 0; i < cart.selections.length; i++) {
+        message += `${i + 1}. ${cart.selections[i].nombre} - $${cart.selections[i].precio}\n`;
+   }
+   message += "Total: $" + cart.totalPrice();
+   alert(message);
+}
+
 const promo = [
-    {nombre: "Fernet 1L con coca 2.25L", precio: 16000},
-    {nombre: "Campari 0.75L con Cepita 1L", precio: 10000},
-    {nombre: "Gin Tanqueray 0.75L con Tónica 2L", precio: 12000},
+    { nombre: "Fernet 1L con coca 2.25L", precio: 16000 },
+    { nombre: "Campari 0.75L con Cepita 1L", precio: 10000 },
+    { nombre: "Gin Tanqueray 0.75L con Tónica 2L", precio: 12000 },
 ];
 
 const drink = [
-    {nombre: "Coca 2.25L", precio: 4500},
-    {nombre: "Fernet 1L", precio: 12000},
-    {nombre: "Campari 0.75L", precio: 8500},
-    {nombre: "Cepita 1L", precio: 2500},
-    {nombre: "Gin 0.75L", precio: 10000},
-    {nombre: "Tónica", precio: 2500},
+    { nombre: "Coca 2.25L", precio: 4500 },
+    { nombre: "Fernet 1L", precio: 12000 },
+    { nombre: "Campari 0.75L", precio: 8500 },
+    { nombre: "Cepita 1L", precio: 2500 },
+    { nombre: "Gin 0.75L", precio: 10000 },
+    { nombre: "Tónica", precio: 2500 },
 ];
 
+
+
+let cart = {
+    selections: [],
+    totalPrice: function () {
+        let suma = 0;
+        for (let i = 0; i < this.selections.length; i++) {
+            suma = suma + this.selections[i].precio;
+        }
+        return suma;
+    }
+
+}
 
 adult();
 
